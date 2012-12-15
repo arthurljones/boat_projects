4905afc31a0ed5b8bc481ed5f1bd8bfd1cf62dd6
Arthur L. Jones IV <arthur.l.jones@gmail.com>
2012-12-15 14:33:00 -0800

    Initial project setup

---
+++ boat_projects/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoatProjects::Application.config.secret_token = 'ca6ee83d184992fcecc62380ddada07bfceeacb369c2d76faf21a7f3a5381b2e5646c94bdd8e14c33239916bf2b9cd21550e4c3321f1d596c994de15771e2fd5'
