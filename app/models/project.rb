2852590692ac73361d1571d2b6cebbbf76b1b0f5
Arthur L. Jones IV <arthur.l.jones@gmail.com>
2013-05-11 12:53:25 -0700

    Move sorting functionality into model

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,8 +8,11 @@ class Project < ActiveRecord::Base
   has_many :services, :through => :project_services
   has_many :dependencies, :through => :projects_dependencies, :class_name => 'Project', :foreign_key => 'dependency_id'
 
-  default_scope order("name asc")
-  scope :incomplete, where{(completed == false) & (obsolete == false)}
+  scope :complete, where{completed == true}
+  scope :incomplete, where{completed == false}
+  scope :obsolete, where{obsolete == true}
+  scope :current, where{obsolete == false}
+  scope :active, incomplete.current
 
   attr_accessible :project_materials_attributes
   accepts_nested_attributes_for :project_materials, :allow_destroy => true
@@ -17,16 +20,35 @@ class Project < ActiveRecord::Base
   attr_accessible :project_services_attributes
   accepts_nested_attributes_for :project_services, :allow_destroy => true
 
-  #sifter :cost { }
-
   def cost
     cost = project_materials.map(&:cost).inject(:+)
     return cost || 0
   end
 
-  def self.total_cost
-    cost = Project.all.map(&:cost).inject(:+)
+  def self.cost
+    cost = all.map(&:cost).inject(:+)
     return cost || 0
   end
 
+  def self.sorted_by(attribute, desc=true)
+    ap attribute
+    ap desc
+    if column_names.include? attribute
+      order_string = "#{attribute} #{desc ? 'desc' : 'asc'}"
+      ap order_string
+      order(order_string).all
+    else
+      result = all.sort_by(&attribute.to_sym)
+      result.reverse! if desc
+      result
+    end
+  end
+
+  def self.sortable_columns
+    column_names + ["cost"]
+  end
+
+  def self.sortable_by?(column)
+    sortable_columns.include?(column)
+  end
 end
