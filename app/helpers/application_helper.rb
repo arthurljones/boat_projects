1d98d8a6ed2014484bc982035a202f7425928bdc
Arthur L. Jones IV <arthur.l.jones@gmail.com>
2013-05-11 12:58:44 -0700

    Clean up sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
   def sortable(model_class, column, title = nil)
     title ||= model_class.human_attribute_name(column)
-    direction = sort_direction == "desc" ? "asc" : "desc"
-    icon = (direction == "asc" ? "icon-chevron-down" : "icon-chevron-up")
-    link = link_to title, {:sort => column, :direction => direction}
-    if (column.to_s == sort_column(model_class).to_s)
-    	puts "Sort Column: #{column}"
-    	return "<i class=#{icon}> #{link}</i>".html_safe
+    is_current_column = (column.to_s == session[:sort])
+    desc = !(is_current_column and session[:desc])
+    link = link_to title, {:sort => column, :desc => desc}
+    if is_current_column
+        icon = session[:desc] ? "icon-chevron-down" : "icon-chevron-up"
+    	"<i class=#{icon}> #{link}</i>".html_safe
     else
-    	return link
+    	link
     end
   end
 end
