bf44610132a7aec69775e8f0888257fbdf5981a4
Arthur L. Jones IV <arthur.l.jones@gmail.com>
2013-05-11 12:57:35 -0700

    Remove sorting functionality from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :sort_column, :sort_direction
-
-  def default_column
-  	"name"
-  end
-
-  def sort_column(model_class)
-    model_class.column_names.include?(params[:sort]) ? params[:sort] : default_column
-  end
-  
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-  end
 end
