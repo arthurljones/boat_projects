82f8095ac321e608bf09f8d8bd556c3731c325a0
Arthur L. Jones IV <arthur.l.jones@gmail.com>
2013-05-12 14:48:24 -0700

    Fix incorrect dependency

--- app/models/task_dependency.rb
+++ app/models/task_dependency.rb
@@ -3,5 +3,5 @@ class TaskDependency < ActiveRecord::Base
   attr_accessible :dependency_id, :task_id
 
   belongs_to :task
-  belongs_to :dependency, :class => "Task", :foreign_key => :dependency_id
+  belongs_to :dependency, :class_name => "Task", :foreign_key => :dependency_id
 end
